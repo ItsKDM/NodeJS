4860e80c2fa45578dcbffd54a613ae1bdd5ec6d4
ItsKDM <darshangupta.0602@gmail.com>
2023-06-11 02:35:18 +0530

    ProductOfTwoNumbers

---
+++ JS_Basics/play.js
@@ -0,0 +1,10 @@
+// let x = 10;
+// let y = 4;
+
+const productOfTwo = (x, y) => {
+    return (
+        "x = " + x + "\ny = " + y + "\nProduct of x and y = " + x*y
+    );
+}
+
+console.log(productOfTwo(8, 6));
