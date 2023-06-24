8505668f0a3db0924b7e2c42ac28fa4ed612daa1
Darshan <51724336+ItsKDM@users.noreply.github.com>
2023-06-25 03:14:00 +0530

    Full_Stack_Expense_Tracker_App

--- ExpressJS/Full_Stack_Expense_Tracker_App/util/database.js
+++ ExpressJS/Full_Stack_Expense_Tracker_App/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expense_app', 'root', 'Application@121', {
+const sequelize = new Sequelize('expense_app', 'root', 'password', {
     dialect: 'mysql',
     host: 'localhost'
 });
